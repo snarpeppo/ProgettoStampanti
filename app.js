0f2d8a88995ac65a46640c0a977966d58af52571
FedericoDagata <federico.dagata98@gmail.com>
2021-02-16 09:27:45 +0100

    prettier e header.ejs

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const lpq = require('./api/lpq.js')
-const lpstat = require('./api/lpstat.js')
+const lpq = require("./api/lpq.js");
+const lpstat = require("./api/lpstat.js");
 
 app.listen(3000);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-
-app.get('/lpq',(req,res) => {
-    const command = lpq();
-    console.log('command',command);
-    res.render('lpqView',{
-        command
-    });
+app.get("/lpq", (req, res) => {
+  const command = lpq();
+  console.log("command", command);
+  res.render("lpqView", {
+    command,
+  });
 });
 
-app.get('/lpstat',(req,res) => {
-    const command = lpstat();
-    console.log('command',command);
-    res.render('lpstatView',{
-        command
-    });
+app.get("/lpstat", (req, res) => {
+  const command = lpstat();
+  console.log("command", command);
+  res.render("lpstatView", {
+    command,
+  });
 });
